3e7c50d4b8c80d1e4cefb9240a9f1ec8aa2a6c24
Pawel Piechowiak <pawelpiecho1992@o2.pl>
2019-07-11 11:15:43 +0200

    Change RestaurantTable to render function.

--- src/components/LeftMenu/RestaurantTable.vue
+++ src/components/LeftMenu/RestaurantTable.vue
@@ -1,31 +1,36 @@
-<template>
-  <button class="btn btn-block m-1" @click="$emit('chosedTable', tableNumber)" :class="getStatus" >
-     <span class="badge badge-pill badge-light">{{tableNumber}}</span>
-      {{assignedTo}}
-  </button>
-</template>
 
 <script>
 export default {
   name: "restaurantTable",
+  functional: true,
+  render: function(createElement, context) {
+    let childrens= [];
+    let span= createElement("span",
+    {
+      domProps: {
+        className: "badge badge-pill badge-ligh",
+        innerHTML: context.props.startData.id
+      }
+    });
+    childrens.push(span);
+    childrens.push(context.props.startData.assignedTo);
+    return createElement('button',
+    {
+      on: {
+        click: e => context.listeners.chosedTable( context.props.startData.id)
+      },
+      class: {
 
+          "btn-outline-success": context.props.startData.assignedTo != '',
+          "btn btn-block m-1 ": true
+
+      }
+    },
+    childrens);
+  },
   props: {
     startData:Object
   },
-  computed: {
-    getStatus: function(){
-      return {
-        "btn-outline-success": this.assignedTo != ''
-      }
-    },
-    tableNumber: function() {
-      return this.startData.id;
-    },
-    assignedTo: function() {
-      return this.startData.assignedTo;
-    }
-  },
-
 
 }
 </script>
