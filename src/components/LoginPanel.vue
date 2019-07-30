ca175d0330161af0b83df47f28a9cb0952c36b0b
Pawel Piechowiak <pawelpiecho1992@o2.pl>
2019-07-30 11:35:01 +0200

    Add render option in LoginPanel

--- src/components/LoginPanel.vue
+++ src/components/LoginPanel.vue
@@ -18,6 +18,77 @@ import {getData} from '../mixins/getData.js'
 
 
 export default {
+  // render: function(createElement, context) {
+  //   console.log(context);
+  //   let data =  {
+  //
+  //         userName:'user',
+  //         userPass: 'pass',
+  //         show: false
+  //
+  //   };
+  //
+  //   let childrens=[];
+  //   let login= createElement('input', {
+  //     domProps:{
+  //       className: 'form-control',
+  //       placeholder: "Login",
+  //     },
+  //     on:{
+  //       input: (e) => {data.userName= e.target.value}
+  //     }
+  //   });
+  //
+  //   let pass= createElement('input', {
+  //     domProps:{
+  //       className: 'form-control',
+  //       placeholder: "Password"
+  //     },
+  //     on:{
+  //       input: (e) => {data.userPass= e.target.value}
+  //     }
+  //   });
+  //
+  //   let warningSpan = createElement('span',{
+  //     domProps:{
+  //       innerHTML: 'Nieprawidłowy login lub password!',
+  //     },
+  //   });
+  //
+  //   let warning= createElement('div',{
+  //     class:{
+  //       // wrongPass
+  //     }
+  //   });
+  //     let okButton= createElement('button',{
+  //       domProps:{
+  //         className: 'btn btn-info btn-block ',
+  //         innerHTML: "Login"
+  //       },
+  //       on:{
+  //         click: ()=>{context.listeners.user(data.userName)}
+  //       }
+  //     });
+  //   childrens.push(login);
+  //   childrens.push(pass);
+  //   childrens.push(okButton);
+  //
+  //   return createElement('div',{
+  //     domProps:{
+  //       className: 'm-auto p-5 shadow border ',
+  //     },
+  //     on: {
+  //       keyup: (e)=>{if(e.keyCode != 13) }
+  //     },
+  //     data : function() {
+  //         return {
+  //           userName:'user',
+  //           userPass: 'pass',
+  //           show: false
+  //         }
+  //     },
+  //   },childrens);
+  // },
   mixins: [getData],
   data : function() {
       return {
@@ -51,5 +122,10 @@ export default {
 </script>
 
 <style scoped>
-
+  /* input {
+    margin-bottom: 10px;
+  }
+  button {
+    margin-top: 50px;
+  } */
 </style>
